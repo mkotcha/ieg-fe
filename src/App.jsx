1aa108c8ea61fafd3e1a7134060082f62c22ecd1
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-30 22:44:10 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Container className="bg-warning p-3">ciao</Container>
+    </>
+  );
+}
+
+export default App;
