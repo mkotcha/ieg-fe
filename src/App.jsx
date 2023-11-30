e8f9ffb72e70854c3527c06fa798df2d3fa55ee1
Matteo Falasca <matteo.falasca@emmek.org>
2023-12-01 00:10:48 +0100

    redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { Container } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Container className="bg-warning p-3">ciao</Container>
---
+++ src/redux/store/index.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: import.meta.env.VITE_REACT_APP_PERSIST_KEY,
+    }),
+  ],
+};
+
+const mainReducer = combineReducers({});
+
+const persistedReducer = persistReducer(persistConfig, mainReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
